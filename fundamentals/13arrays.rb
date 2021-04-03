717c2dba3c1bffa59a2ddba27f0a2f423f3df5cd
Kevin Narain <kevinnarain94@gmail.com>
2021-04-03 03:15:30 +0200

    wowzers

---
+++ fundamentals/13arrays.rb
@@ -0,0 +1,6 @@
+# Arrays
+friends = Array["Kevin", "Karen", "Oscar"]
+puts friends.length() # how many indexes are in arr?
+puts friends.include? "Karen" # does arr include Karen?
+puts friends.reverse() # reverse order arr
+puts friends.sort() # sort a-z, can't sort num, strings together
---
+++ fundamentals/14hashes.rb
@@ -0,0 +1,6 @@
+# Hashes -> Associative Array, key => value, dictionary
+states = {
+  1 => "Hyuna",
+  2 => "Kim"
+}
+puts states[1] # prints value inside key
